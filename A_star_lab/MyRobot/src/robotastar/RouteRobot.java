de897c351cd2b3b5b1eef30e5a1b30b7aed1459b
alvaro-lloret <61242108+alvaro-lloret@users.noreply.github.com>
2020-04-08 20:16:40 +0200

    Update RouteRobot.java

--- A_star_lab/MyRobot/src/robotastar/RouteRobot.java
+++ A_star_lab/MyRobot/src/robotastar/RouteRobot.java
@@ -1,20 +1,42 @@
 package robotastar;
 import robocode.Robot;
 import robocode.ScannedRobotEvent;
+import static robocode.util.Utils.normalRelativeAngleDegrees;
+import java.awt.Color;
 
 public class RouteRobot extends Robot{
 	
-	 public void run() {
-	   turnLeft(getHeading()%90);
-	   turnGunRight(90);
-	   while(true) {
-	      ahead(1000);
-	      turnRight(90);
-	    }
-	 }
+	public void run() {
+		
+		
+		// Color of our robot
+		setAllColors(Color.red);
+
+		//Data that must be equal to the data in the Main class RouteFinder
+		long seed = 0;
+		int NumTileRows = 13;
+		int NumTileCols = 10;
+		int NumObstacles = 39;
+		int PixelPerTile = 64;
+		
+		
+		//robot is initially looking at the top
+		turnRight(normalRelativeAngleDegrees(0 - getHeading()));
+		
+		//Our robot will turn around itself a little bit		
+		int k = 0;
+		while(k < 20){
+			turnRight(90);
+			k++;
+		}
+		
+		
+		// We must:
+		//  1. Generate the search problem
+		//  2. Find the solution with a search algorithm
+		//  3. Execute the solution found
+			
+	}
 	 
-	 public void onScannedRobot(ScannedRobotEvent e) {
-	   fire(1);
-	 }
 
 }
